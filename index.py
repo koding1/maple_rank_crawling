8ae3092e9f8942124c709912724431e8f6719af4
koding1 <koding1@naver.com>
2020-12-27 14:36:56 +0000

    plus job

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import time
 
 
 # j, d, page 순서
-job = {"히어로" : [1, 12, 28], "팔라딘" : [1, 22, 18], "다크나이트" : [1, 32, 18]}
+job = {"히어로" : [1, 12, 28], "팔라딘" : [1, 22, 18], "다크나이트" : [1, 32, 18] , "불독" : [2, 12, 73], "썬콜" : [2, 22, 18], "비숍" : [2, 32, 18]}
 # 유저 설정
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
 
@@ -17,7 +17,7 @@ def page_serch(val):
     d = val[1]
     page_num = val[2]
     
-    while page_num <= 50:
+    while page_num <= 200:
         print(page_num, "페이지를 탐색 합니다.")
         # 카이저 랭킹 페이지 (1)
         url = 'https://maplestory.nexon.com/Ranking/World/Total?page=' + str(page_num) + '&j=' + str(j) + '&d=' + str(d)
@@ -30,7 +30,7 @@ def page_serch(val):
         data = soup.select('.rank_table > tbody > tr')
               
         if int(str(data[9].select('td')[2])[7:10]) >= FIND_LEVEL:
-            page_num += 1
+            page_num += 5
         else:
             i = 8
             while i >= 0:
